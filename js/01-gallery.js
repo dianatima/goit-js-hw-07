7310a0f3a00120e0adea4a7ee1d3270904980e0c
nayanzina@gmail.com <wwwtima.cherin@gmail.com>
2022-11-17 01:05:05 +0200

    hw-7

---
+++ js/01-gallery.js
@@ -0,0 +1,45 @@
+import { galleryItems } from './gallery-items.js';
+// import * as basicLightbox from 'basiclightbox';
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const makeGalleryElMarkup = ({preview, original, description}) => {
+
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+};
+
+const makeGallery = galleryItems.map(makeGalleryElMarkup).join('');
+
+gallery.insertAdjacentHTML('beforeend', makeGallery);
+
+function  onImageClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return
+    };
+    
+    const img = event.target;
+    console.log(img);
+    const imgUrl = img.getAttribute('data-source');
+
+    const instance = basicLightbox.create(`
+    <img src="${imgUrl}" width="800" height="600">
+`);
+
+instance.show();
+
+};
+
+gallery.addEventListener('click', onImageClick);
